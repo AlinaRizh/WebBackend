4fb40b85cdb128b8e8fe560426607b8991e56a21
AlinaMacBook <gaevskaya_alina26@mail.ru>
2025-05-05 16:41:57 +0300

    fix final

--- web6/admin.php
+++ web6/admin.php
@@ -41,17 +41,6 @@ try {
         PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
     ]);
 
-    $stmt = $db->prepare("SELECT * FROM admins WHERE login = :login");
-    $stmt->execute([':login' => $_SERVER['PHP_AUTH_USER']]);
-    $admin = $stmt->fetch();
-
-    if (!$admin || !password_verify($_SERVER['PHP_AUTH_PW'], $admin['password'])) {
-        header('WWW-Authenticate: Basic realm="Restricted Area"');
-        header('HTTP/1.0 401 Unauthorized');
-        echo 'Неверные учетные данные.';
-        exit;
-    }
-
     if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         if (isset($_POST['delete'])) {
             $db->beginTransaction();
